ac18382de0f119d16ce714ce77410321ea60ca3a
Adan <adanasandoval@gmail.com>
2014-09-26 19:30:17 -0500

    Cleaned up some code

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
-urlpatterns = patterns('snippets.views',
-    url(r'^snippets/$', 'snippet_list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', 'snippet_detail'),
+from snippets import views
+
+urlpatterns = patterns('',
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
                        )
+
 urlpatterns = format_suffix_patterns(urlpatterns)
